1f4535b967adba93aeffa2ad497011e3ddd24360
Orjan Ameye <orjan.ameye@hotmail.com>
2025-05-02 20:17:46 +0200

    feat: disconnected graphs (#15)

---
+++ test/unconnected_graphs.jl
@@ -0,0 +1,10 @@
+using GraphCombinatorics, Graphs, Test
+
+@testset "first order ϕ⁴" begin
+    n = [2, 0, 0, 1]
+    topologies = allgraphs(n; connected=false)
+    @test length(topologies) == 2
+
+    graph, _ = topologies[2]
+    @test !is_connected(build_graph(graph))
+end
