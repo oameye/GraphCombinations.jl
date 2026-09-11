f69da08b044892c6e52060d54ba50ee44388db40
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-11 15:25:29 +0200

    research: include colored-port pruning helper

--- src/GraphCombinations.jl
+++ src/GraphCombinations.jl
@@ -15,6 +15,7 @@ include("direct_generation.jl")
 include("row_state_reduction.jl")
 include("generation.jl")
 include("colored_port_generation.jl")
+include("colored_port_pruning.jl")
 
 export allgraphs, combinatoric_factor, build_graph, total_degree, canonical_form
 
