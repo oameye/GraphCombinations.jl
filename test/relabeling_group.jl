d8f3b1c01d7e466ad32bbf199e499e8b4e7cf162
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-13 17:09:16 +0200

    style: apply JuliaFormatter to relabeling tests

--- test/relabeling_group.jl
+++ test/relabeling_group.jl
@@ -6,14 +6,8 @@ const GCRelabeling = GraphCombinations
 @testset "problem-preserving relabeling groups" begin
     @testset "full color-cell permutation group" begin
         relabelings = @inferred GCRelabeling._problem_relabelings([1, 1, 1], 0, _ -> true)
-        @test relabelings == [
-            [1, 2, 3],
-            [1, 3, 2],
-            [2, 1, 3],
-            [2, 3, 1],
-            [3, 1, 2],
-            [3, 2, 1],
-        ]
+        @test relabelings ==
+            [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
     end
 
     @testset "fixed prefix and color cells" begin
@@ -21,12 +15,7 @@ const GCRelabeling = GraphCombinations
         @test fixed == [[1, 2, 3], [1, 3, 2]]
 
         colored = @inferred GCRelabeling._problem_relabelings([1, 2, 1, 2], 0, _ -> true)
-        @test colored == [
-            [1, 2, 3, 4],
-            [1, 4, 3, 2],
-            [3, 2, 1, 4],
-            [3, 4, 1, 2],
-        ]
+        @test colored == [[1, 2, 3, 4], [1, 4, 3, 2], [3, 2, 1, 4], [3, 4, 1, 2]]
     end
 
     @testset "problem-preserving predicate restricts the group" begin
