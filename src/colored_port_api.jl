98cb782c88978594ee724ede82dd9b915cd8b0cd
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-14 14:37:15 +0200

    style: format compact public selector

--- src/colored_port_api.jl
+++ src/colored_port_api.jl
@@ -277,9 +277,7 @@ function _public_port_results(
 end
 
 function _generate_weighted_with_stats(
-    problem::ColoredPortProblem,
-    ::AcceptAllPortPolicy,
-    ::MultiplicityPortTransport,
+    problem::ColoredPortProblem, ::AcceptAllPortPolicy, ::MultiplicityPortTransport
 )::Tuple{Vector{WeightedPortCompletion},PortGenerationStats}
     results, stats = _weighted_port_matchings_compact_with_stats(problem._problem)
     return _public_port_results(results), PortGenerationStats(stats)
