3ca49f656fea3e8bd2fb99d612fb11fac45c3c37
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-18 16:42:33 +0200

    fix: use static levelwise include graph

--- src/directed_levelwise/DirectedLevelwise.jl
+++ src/directed_levelwise/DirectedLevelwise.jl
@@ -1,5 +1,8 @@
 module DirectedLevelwise
 
+include("packed_incremental_refinement_trace.jl")
+include("packed_levelwise_workspace.jl")
+include("packed_levelwise_frontier_orbits.jl")
 include("packed_incremental_child_refinement.jl")
 
 end # module DirectedLevelwise
